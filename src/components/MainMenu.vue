9a57a20b5e3b6209ed704d891420560c19134504
pkstoy13 <pkstoyanov1@gmail.com>
2025-12-07 19:09:53 -0500

    understanding vue better, started on menu with styling and font

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="main-style silkscreen-regular">
+    <h1>Wordle with Friends</h1>
+  </div>
+</template>
+
+<style scoped>
+.main-style {
+  background-color: var(--background-color);
+  color: var(--text-color);
+}
+.silkscreen-regular {
+  font-family: 'Silkscreen', sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+.silkscreen-bold {
+  font-family: 'Silkscreen', sans-serif;
+  font-weight: 700;
+  font-style: normal;
+}
+</style>
