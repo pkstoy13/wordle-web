7f992a39c351c39600928a3576094a1ad7cc0562
pkstoy13 <pkstoyanov1@gmail.com>
2025-12-16 12:36:43 -0500

    added navbar to play page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bar">
+    <div class="bar-content">
+      <span>Wordle</span>
+      <span>Friends</span>
+      <span>About</span>
+      <span>Sign-In</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bar {
+  background-color: var(--primary-color);
+  height: 3rem;
+}
+
+.bar-content {
+  display: flex;
+  gap: 3rem;
+  height: 3rem;
+  justify-content: end;
+  align-items: center;
+  margin-right: 1rem;
+}
+
+.bar-content span:first-child {
+  position: absolute;
+  left: 10px;
+}
+</style>
--- src/components/PlayPage.vue
+++ src/components/PlayPage.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import NavBar from './NavBar.vue'
+</script>
+
 <template>
-  <div>Welcome to Play Page</div>
+  <NavBar />
+  <div class="title">Welcome to Play Page</div>
 </template>
+
+<style scoped>
+.title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
